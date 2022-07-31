63482a3c11bddcb40442ca9385e90e5eaf76cdda
Zhora11111 <96517015+Zhora11111@users.noreply.github.com>
2022-07-31 10:57:00 +0300

    Add files via upload

---
+++ cpp/test/simple_class.h
@@ -0,0 +1,51 @@
+#pragma once
+#include "string"
+
+using std::string;
+
+class SimpleClass
+{
+private:
+    string _name;
+
+
+public:
+    SimpleClass() = default;
+
+    explicit SimpleClass(string name) : _name(std::move(name)) {}
+
+    SimpleClass(const SimpleClass& right)
+    {
+        _name = right._name;
+    }
+
+    SimpleClass(SimpleClass&& right) noexcept
+    {
+        _name = std::move_if_noexcept(right._name);
+    }
+
+    SimpleClass& operator=(const SimpleClass& right)
+    {
+        _name = right._name;
+        return *this;
+    }
+
+    SimpleClass& operator=(SimpleClass&& right) noexcept
+    {
+        _name = std::move_if_noexcept(right._name);
+        return *this;
+    }
+
+
+public:
+    _NODISCARD string name() const noexcept
+    {
+        return _name;
+    }
+
+    void name(const string& name) noexcept
+    {
+        _name = name;
+    }
+
+};
